894999b0b344b0b03889eead9c0ea8851df29e88
blackender <taotaomails@gmail.com>
2020-02-03 13:31:37 +0800

    delete JWB_Server

--- JWB_Server/medical_resources/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-# Register your models here.
-from medical_resources.models import MedicalSuppliesType, UserInfo, MedicalSupplies
-
-admin.site.register([MedicalSuppliesType, MedicalSupplies, UserInfo])
